b299720bc8328b3259eec8620082a484669b07b0
Jeremy Turner <Jeremy.Turner@waterstreetcompany.com>
2024-03-20 15:59:44 -0600

    Added a table based route

--- angular-sandbox/src/app/app-routing.module.ts
+++ angular-sandbox/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CommonTableComponent } from './common/common-table/common-table.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'common-table', component: CommonTableComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- angular-sandbox/src/app/app.module.ts
+++ angular-sandbox/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CommonTableComponent } from './common/common-table/common-table.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CommonTableComponent
   ],
   imports: [
     BrowserModule,
